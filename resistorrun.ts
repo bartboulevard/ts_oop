790ff43a7e9755db228b4c5339134470c64cefa7
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2023-01-12 09:08:23 +0200

    ül TS-2: võimsuse arvutamine

--- resistorrun.ts
+++ resistorrun.ts
@@ -6,8 +6,21 @@ class Resistor {
     getCurrent(u: number): number {
         return u / this.r;
     }
-}
+    getPower(u: number): number{
+        return u * this.r;
+    }
+    getAmperage(w: number, v: number): number {
+        return w / v
+    }
+    getResistance(w: number, v: number ): number {
+        return v / this.getAmperage(w, v)
+    }
+    getTemp(w: number, ml: number): number {
+        return Math.floor(20+(60/(4.19*(ml/w))))
+   }
 
+}
+/*
 let resistors: Resistor[] = [];
 let r1: Resistor = new Resistor(110)
 console.log(r1.getCurrent(5))
@@ -22,3 +35,13 @@ let totalCurrent: number = 0;
 resistors.forEach((resistor) => {totalCurrent += resistor.getCurrent(5); });
 console.log(resistors)
 console.log(totalCurrent)
+ */
+
+let r1 = new Resistor(2);
+console.log(r1.getPower(5));
+console.log(r1.getAmperage(6,4))
+console.log(r1.getResistance(6,4))
+console.log(r1.getTemp(1000,1000))
+console.log(r1.getAmperage(1000, 220))
+console.log(r1.getResistance(1000,220))
+
