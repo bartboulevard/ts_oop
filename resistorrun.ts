158a0037c301f5e4ac584d952b81c79c51f0f0cd
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2023-01-13 09:37:04 +0200

    ül TS-4: jadaühenduse arvutamine

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,44 +1,54 @@
 class Resistor {
     r: number = 0;
-    maxPower: number = 0;
-    maxVoltage: number = 0;
 
-    constructor(r: number, maxPower: number, maxVoltage: number) {
+    constructor(r: number) {
         this.r = r;
-        this.maxPower = maxPower;
-        this.maxVoltage = maxVoltage;
     }
 
     getCurrent(u: number): number {
         return u / this.r;
     }
+
     getPower(u: number): number {
         return u * this.getCurrent(u);
     }
-    getMaxPower(): number {
-        return this.maxPower;
+
+    getResistance(): number {
+        return this.r;
     }
-    voltageCheck(u: number): boolean {
-        return u <= this.maxVoltage
+
+}
+
+class SeriesCircuit {
+    resistors: Resistor[] = []
+    push(r: Resistor) {
+        this.resistors.push(r);
+    }
+    getTotalResistance() {
+        let sum: number = 0;
+        this.resistors.forEach((r: Resistor) => {sum += r.getResistance() });
+        return sum;
+    }
+    getCurrent(u: number) {
+        return u / this.getTotalResistance();
+    }
+    getPower(u: number) {
+        return Math.pow((u), 2) / this.getTotalResistance(); // funktsioon küsib rakendatud pinget, et arvutada võimsus
     }
 }
 
-let r1 = new Resistor(220, 100, 20);
-console.log(r1.voltageCheck(14)); // true, sest max voltage alla 20
-console.log(r1.voltageCheck(32)); // false, sest max voltage üle 20
+let sc1: SeriesCircuit = new SeriesCircuit();
+sc1.push(new Resistor(220));
+sc1.push(new Resistor(220));
+sc1.push(new Resistor(220));
+console.log("Esimese jadaühenduse kogutakistus: " + sc1.getTotalResistance());
+console.log("Esimese jadaühenduse pinge: " + sc1.getCurrent(12))
+console.log("Esimese jadaühenduse võimsus: " + sc1.getPower(12))
 
-const resistors: Resistor[] = [
-    new Resistor(220, 100, 10),
-    new Resistor(330, 200, 15),
-    new Resistor(100, 50, 5),
-    new Resistor(100, 70, 8)
-];
-
-function getAllowedResistor(voltage: number, resistors: Resistor[]): Resistor[] {
-    return resistors.filter(resistor => resistor.voltageCheck(voltage));
-
-}
-let allowedResistor = getAllowedResistor(8, resistors); // output = 3, sest üks alla 8 voldi
-console.log(allowedResistor)
-
-
+let sc2: SeriesCircuit = new SeriesCircuit();
+sc2.push(new Resistor(220))
+sc2.push(new Resistor(220))
+sc2.push(new Resistor(110))
+console.log("Teise jadaühenduse kogutakistus: " + sc2.getTotalResistance())
+console.log("Teise jadaühenduse pinge: " + sc2.getCurrent(12))
+console.log("Teise jadaühenduse võimsus: " + sc2.getPower(12))
